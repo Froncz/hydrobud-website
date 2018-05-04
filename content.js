9b7769abbf897d15a75c1f57842b3d00f93f9a9a
froncz <aleksander.froncz@gmail.com>
2018-05-04 18:13:30 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 window.content = {
   "o_nas": {
-    "tytul": "Firma test",
+    "tytul": "Firma",
     "o_firmie": "Firma Hydrobud powstała w 1992 roku pod nazwą Zakład Instalacji Sanitarnych C.O. i Gaz. W 2002 roku nastąpiła zmiana nazwy na obecną. W ciągu 25 lat działalności zrealizowaliśmy szereg inwestycji dla klientów instytucjonalnych i indywidualnych na terenie całego kraju. Specjalizujemy się w wykonawstwie instalacji hydraulicznych, wodno-kanalizacyjnych, centralnego ogrzewania, wentylacji i klimatyzacji. Posiadamy kompetentną kadrę oraz niezbędny sprzęt do wykonywania powierzonych prac. Priorytetem firmy jest maksymalizacja satysfakcji klientów.",
     "oferta_tytul": "Oferta",
     "zakres1_tytul": "Naszą specjalnością są instalacje wewnętrzne:",
